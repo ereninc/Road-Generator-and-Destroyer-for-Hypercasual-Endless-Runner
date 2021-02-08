855f041e5a901c7f830180e4f9e05c3c654f97d4
Eren Inanci <erencan_inanci@hotmail.com>
2021-02-08 04:28:37 +0300

    fin

---
+++ Script/DestroyRoad.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyRoad : MonoBehaviour
+{
+    Rigidbody fizik;
+    [SerializeField] private float Hiz = 10;
+    
+	void Start()
+    {
+        fizik = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        fizik.velocity = transform.forward * Hiz;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "yolTag")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Script/RoadGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+public class RoadGenerator : MonoBehaviour
+{
+    [SerializeField] private GameObject[] RoadPrefabs;
+    [SerializeField] private GameObject player;
+    [SerializeField] private float nextStep = 0;
+    [SerializeField] private Transform startPosition;
+    
+
+    void Start()
+    {
+        StartCoroutine(GenerateRoad());
+    }
+
+    IEnumerator GenerateRoad()
+    {
+        while (player.transform.position.y >= -0.140f)
+        {
+            GameObject randomRoadPrefab = RoadPrefabs[Random.Range(0, RoadPrefabs.Length)];
+            Instantiate(randomRoadPrefab, new Vector3(startPosition.position.x, startPosition.position.y, startPosition.position.z + nextStep), randomRoadPrefab.transform.rotation);
+            nextStep += 65;
+            yield return new WaitForSeconds(2);
+        }
+    }
+}
